f5d5bf07edda46584aa5e26b4428b7251d750a07
covolan <covolan09@gmail.com>
2024-06-26 14:28:18 -0300

    Added pagination to products

--- src/Pages/ProductsCategory.jsx
+++ src/Pages/ProductsCategory.jsx
@@ -1,41 +1,56 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./CSS/ProductsCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import Item from "../Components/Item/Item";
+import Pagination from "@mui/material/Pagination";
 
 function ProductsCategory(props) {
   const { productData } = useContext(ShopContext);
-  let totalProducts = 0;
+  const products = productData.filter(
+    (prod) => prod.category == props.category
+  );
+  const PRODUCTS_PER_PAGE = 4;
+  const QUANTITY_OF_PAGES = Math.ceil(products.length / PRODUCTS_PER_PAGE);
+  const [displayProd, setDisplayProd] = useState(
+    products.slice(0, PRODUCTS_PER_PAGE)
+  );
+  const [page, setPage] = useState(1);
 
-  const countTotal = () => {
-    productData.map((item) => {
-      if (props.category == item.category) {
-        totalProducts += 1;
-      }
-    });
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    const totalItems = PRODUCTS_PER_PAGE * value;
+    setDisplayProd(products.slice(totalItems - 4, totalItems));
   };
 
-  // TODO: Add pagination with material ui
-  // TODO: Create API to add products
-  // pagination example: https://hygraph.com/blog/react-pagination
+  useEffect(() => {
+    setPage(1);
+    setDisplayProd(products.slice(0, PRODUCTS_PER_PAGE));
+  }, [props.category]);
 
-  countTotal();
+  // TODO: Create API to add products
 
   return (
     <div className="category">
       <div className="category-header">
         <p>
-          Showing {totalProducts} out of {totalProducts} products
+          Showing {displayProd.length} out of {products.length} products
         </p>
         <button>Sort by</button>
       </div>
       <div className="category-products">
-        {productData.map((item, index) => {
-          if (props.category == item.category) {
-            return <Item key={index} product={item} />;
-          }
-          return null;
+        {displayProd.map((item, index) => {
+          return (
+            <Item key={index + item.productName + Date()} product={item} />
+          );
         })}
+      </div>
+      <div className="pagination">
+        <Pagination
+          count={QUANTITY_OF_PAGES}
+          onChange={handlePageChange}
+          page={page}
+          shape="rounded"
+        />
       </div>
     </div>
   );
