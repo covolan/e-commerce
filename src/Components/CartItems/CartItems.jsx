4cdb0ca468110f81f65e4657dca280e205ba84c6
covolan <covolan09@gmail.com>
2024-06-27 23:35:19 -0300

    Started the cart component development

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,47 +1,50 @@
 import React, { useContext } from "react";
 import "./CartItems.css";
 import { ShopContext } from "../../Context/ShopContext";
+import cancelIcon from "../assets/cancel.svg";
 
 function CartItems() {
   const { productData, cartItems, delFromCart } = useContext(ShopContext);
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Image</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Total</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {productData.map((item) => {
-          if (cartItems[item.id] > 0) {
-            return (
-              <tr key={item.productName}>
-                <td>{item.productName}</td>
-                <td>
-                  <img className="product-image" src={item.image[0]} alt="" />
-                </td>
-                <td>{item.price}</td>
-                <td>{cartItems[item.id]}</td>
-
-                <td>{item.price * cartItems[item.id]}</td>
-                <td
-                  onClick={() => {
-                    delFromCart(item.id);
-                  }}
-                >
-                  X
-                </td>
-              </tr>
-            );
-          }
-        })}
-      </tbody>
-    </table>
+    <div className="cart">
+      <h1 className="cart-title">Your cart Has X items</h1>
+      <table>
+        <thead>
+          <tr>
+            <th className="product-th">Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productData.map((item) => {
+            if (cartItems[item.id] > 0) {
+              return (
+                <tr key={item.productName}>
+                  <td className="product-td">
+                    <img className="product-image" src={item.image[0]} alt="" />
+                    {item.productName}
+                  </td>
+                  <td>{`$${item.price}`}</td>
+                  <td>{cartItems[item.id]}</td>
+                  <td className="total">
+                    {`$${item.price * cartItems[item.id]}`}
+                    <img
+                      className="cancel"
+                      onClick={() => {
+                        delFromCart(item.id);
+                      }}
+                      src={cancelIcon}
+                    />
+                  </td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
