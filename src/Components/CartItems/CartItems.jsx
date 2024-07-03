cdde25bea59b1ed6749fb634e131bf6b4d6f3c70
covolan <covolan09@gmail.com>
2024-07-02 21:22:49 -0300

    Created empty cart page

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -10,23 +10,38 @@ function CartItems() {
 
   const taxes = 0.15;
 
-
   const runTotal = () => {
     let tempTotal = 0;
     let tempCartTotalItems = 0;
-    productData.map(item => {
-      if(cartItems[item.id] > 0) {
+    productData.map((item) => {
+      if (cartItems[item.id] > 0) {
         tempTotal += item.price * cartItems[item.id];
         tempCartTotalItems += cartItems[item.id];
       }
-    })
+    });
     setTotalValue(tempTotal);
     setCartTotalItems(tempCartTotalItems);
-  }
+  };
 
   useEffect(() => {
     runTotal();
-  }, [cartItems])
+  }, [cartItems]);
+
+  if (cartTotalItems == 0) {
+    return (
+      <div className="empty-cart">
+        <span class="material-symbols-outlined icon-cart">
+          sentiment_dissatisfied
+        </span>
+        <h1 className="empty-cart-title">
+          Oops.. It seems that you didn't added anything yet!
+        </h1>
+        <h2 className="empty-cart-subtitle">
+          Feel free to explore our store and find what you need!
+        </h2>
+      </div>
+    );
+  }
 
   return (
     <div className="cart">
@@ -80,14 +95,14 @@ function CartItems() {
             </tr>
             <tr>
               <td className="total-table-emph grand-total">Grand Total:</td>
-              <td className="total-table-amount grand-total">${(totalValue * taxes) + totalValue}</td>
+              <td className="total-table-amount grand-total">
+                ${totalValue * taxes + totalValue}
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
-        <button className="checkout-btn">
-          Check out
-        </button>
+      <button className="checkout-btn">Check out</button>
     </div>
   );
 }
