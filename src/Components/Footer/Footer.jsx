17e26b7f21fe97a0b1151be0eb36e6278e243f26
covolan <covolan09@gmail.com>
2024-07-03 18:06:49 -0300

    Restylished the footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,30 +1,53 @@
 import React from "react";
-import "./Footer.css"
-import instagramLogo from "../assets/instagram.svg"
-import whasappLogo from "../assets/whatsapp.svg"
-import twitterLogo from "../assets/logo-twitter.svg"
+import "./Footer.css";
+import instagramLogo from "../assets/instagram.svg";
+import whasappLogo from "../assets/whatsapp.svg";
+import twitterLogo from "../assets/logo-twitter.svg";
 
+const firstLinks = ["Contact", "Carrers", "Company", "FAQ", "SAC"];
+const secondLinks = ["Home", "Gaming", "Office", "Login", "Go to Top"];
+const socialLinks = [instagramLogo, whasappLogo, twitterLogo];
 
 function Footer() {
   return (
     <div className="footer">
-      <hr />
       <h2 className="footer-title">INFORMATIKA</h2>
-      <ul className="footer-links">
-        <li>Home</li>
-        <li>Contact</li>
-        <li>Carrrers</li>
-        <li>Company</li>
-      </ul>
-      <ul className="footer-socials">
-        <li><img className="icon" src={instagramLogo} alt="" /></li>
-        <li><img className="icon" src={whasappLogo} alt="" /></li>
-        <li><img className="icon" src={twitterLogo} alt="" /></li>
-      </ul>
+      <div className="footer-left">
+        <ul className="footer-links-list">
+          {firstLinks.map((item, index) => (
+            <li key={index + item} className="footer-links">
+              {item}
+            </li>
+          ))}
+        </ul>
+        <ul className="footer-links-list">
+          {secondLinks.map((item, index) => (
+            <li key={index + item} className="footer-links">
+              {item}
+            </li>
+          ))}
+        </ul>
+        <ul className="footer-socials">
+          {socialLinks.map((item, index) => (
+            <li key={index + item}>
+              <img className="icon-socials" src={item} alt="" />
+            </li>
+          ))}
+        </ul>
+        <div className="footer-right">
+          <p className="footer-email-msg">Send us a email</p>
+          <div className="footer-email-div">
+            <input
+              className="footer-email"
+              placeholder="email@hoster.com"
+              type="email"
+            />
+            <button className="footer-send-btn">send</button>
+          </div>
+        </div>
+      </div>
       <hr />
-        <p className="footer-copyright">
-            Copyright @ 2024 - All Right Reserved
-        </p>
+      <p className="footer-copyright">Copyright @ 2024 - All Right Reserved</p>
     </div>
   );
 }
